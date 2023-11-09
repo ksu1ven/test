42ef6b47d80386cda903d94ec14d5d18e1bb4157
dayanych <dayana.shakenova20@gmail.com>
2023-11-09 21:49:31 +0600

    feat: add 404

--- src/components/pokemon-card/pokemon-card.tsx
+++ src/components/pokemon-card/pokemon-card.tsx
@@ -19,53 +19,67 @@ const PokemonCard = () => {
     setIsLoading(true);
     if (name) {
       const pokemonApi = new PokeApi();
-      const convertPokemon = await pokemonApi.getPokemon(name);
-      convertPokemon && setPokemon(convertPokemon);
+      try {
+        const convertPokemon = await pokemonApi.getPokemon(name);
+        convertPokemon && setPokemon(convertPokemon);
+      } catch (error) {
+        navigate("404");
+      } finally {
+        setIsLoading(false);
+      }
     }
     setIsLoading(false);
   };
 
   return (
     <div className="pokemon-card-wrapper" onClick={() => navigate(-1)}>
-      {isLoading && <Loader />}
-      {pokemon && (
-        <div className="pokemon-card" onClick={(e) => e.stopPropagation()}>
-          <button className="close-button" onClick={() => navigate(-1)}>
-            X
-          </button>
-          <h2 className="pokemon-name">{pokemon.name}</h2>
-          <div className="pokemon-main-info">
-            <div className="pokemon-images">
-              {pokemon.image.map((image) => (
-                <div key={image.url} className="pokemon-image-wrapper">
-                  {image.url ? (
-                    <img
-                      src={image.url}
-                      alt={pokemon.name}
-                      className={image.type}
-                    />
-                  ) : (
-                    <div style={{ color: "black" }}>No {image.type} image</div>
-                  )}
-                </div>
-              ))}
-            </div>
+      <div className="pokemon-card" onClick={(e) => e.stopPropagation()}>
+        <button className="close-button" onClick={() => navigate(-1)}>
+          X
+        </button>
+        {isLoading && <Loader />}
+        {pokemon && (
+          <>
+            <h2 className="pokemon-name">{pokemon.name}</h2>
+            <div className="pokemon-main-info">
+              <div className="pokemon-images">
+                {pokemon.image.map((image) => (
+                  <div key={image.url} className="pokemon-image-wrapper">
+                    {image.url ? (
+                      <img
+                        src={image.url}
+                        alt={pokemon.name}
+                        className={image.type}
+                      />
+                    ) : (
+                      <div style={{ color: "black" }}>
+                        No {image.type} image
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
 
-            <div>
-              <p>
-                Weight:{" "}
-                <span className="pokemon-weight">{pokemon.weight / 10} kg</span>
-              </p>
-              <p>
-                Height:{" "}
-                <span className="pokemon-height">{pokemon.height * 10} sm</span>
-              </p>
-              <p>Is default: {pokemon.isDefault ? "yes" : "no"}</p>
-              <p>Abilities: {pokemon.abilities.join(", ")}</p>
+              <div>
+                <p>
+                  Weight:{" "}
+                  <span className="pokemon-weight">
+                    {pokemon.weight / 10} kg
+                  </span>
+                </p>
+                <p>
+                  Height:{" "}
+                  <span className="pokemon-height">
+                    {pokemon.height * 10} sm
+                  </span>
+                </p>
+                <p>Is default: {pokemon.isDefault ? "yes" : "no"}</p>
+                <p>Abilities: {pokemon.abilities.join(", ")}</p>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
